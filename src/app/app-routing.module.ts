6ee3ba7562d4ad082d994196787986d3709c583b
aakashverma1124 <info.aakash11@gmail.com>
2020-11-26 09:56:55 +0530

    routing added and some designing is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Router, RouterModule, Routes} from '@angular/router';
+import {UserListComponent} from './user-list/user-list.component';
 
 
+const routes: Routes = [
+  {path: 'users', component: UserListComponent},
+  {path: '', redirectTo: 'users', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
